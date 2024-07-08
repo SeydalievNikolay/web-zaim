fb00c563833c5552d6acf6671e48ca6acb9cd0c1
nk.seidaliev <nk.seidaliev@mish.design>
2024-07-08 13:28:46 +0700

    init

---
+++ web-zaim/src/main/java/com/example/web_zaim/WebZaimApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_zaim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebZaimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebZaimApplication.class, args);
+	}
+
+}
---
+++ web-zaim/src/test/java/com/example/web_zaim/WebZaimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_zaim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebZaimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
